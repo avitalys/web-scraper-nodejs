bc2e341fcff7acb761fa57ebf8a850587a8fbb6d
Avitalys <avitalyo52@gmail.com>
2024-05-28 19:59:01 +0300

    Added simple caching on /news route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const expressCache = require("cache-express");
 const bodyParser = require("body-parser");
 const scraper = require("./routes/scrape");
 const { auditLoggingMiddleware } = require("./middlewares/audit-logging");
@@ -8,25 +9,38 @@ var cors = require("cors");
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+// cacheing middleWare configuration with options
+let cacheOptions = expressCache({
+  // dependsOn: () => [postCount],
+  //timeOut: 40000, // default 1 hour
+  onTimeout: (key, value) => {
+    console.log(`Cache removed for key: ${key}`);
+  },
+});
+
+// setting CORS headers options
 var corsOptions = {
   // origin: "http://example.com",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-// app.use(cors(corsOptions)); // for all routes
+// middleware for all routes
+// app.use(cors(corsOptions));
 
-// create application/json parser
-var jsonParser = bodyParser.json();
-app.use(jsonParser);
+// create application/json parser middleware
+app.use(bodyParser.json());
 
-// auditing
+// auditing middlware
 app.use(auditLoggingMiddleware);
 
+// console.error middlware
+app.use((err, req, res, next) => {
+  console.error(err);
+});
+
+// Content-Type for /scrape route
 app.use("/scrape", (req, res, next) => {
   res.header("Content-Type", "application/json");
   next();
-});
-app.use((err, req, res, next) => {
-  console.error(err);
 });
 
 app
@@ -42,7 +56,7 @@ app
 
 app.get(
   "/news/:category/:rows(\\d+)?",
-  cors(corsOptions),
+  [cors(corsOptions), cacheOptions],
   function (req, res, next) {
     scraper
       .scrapeSourcesListAsync(null, req.params.category, req.params?.rows)
